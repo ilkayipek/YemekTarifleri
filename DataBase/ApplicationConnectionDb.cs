a0e80fbcf514b669f06c6a48ea4799c3f5f8e2b5
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-04-29 22:38:47 +0300

    ApplicationConnectionDb class created for mysql connection

---
+++ DataBase/ApplicationConnectionDb.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using YemekTarifleri.DataBase;
+//using Microsoft.Extensions.Logging;
+
+
+namespace YemekTarifleri.DataBase
+{
+    public class ApplicationConnectionDb : DbContext
+    {
+        public DbSet<Kategori> kategoriler { get; set; }
+        public DbSet<Kullanici> kullanicilar { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseMySql("server=database-1.cgoekeddamxk.eu-north-1.rds.amazonaws.com;port=3306;user=admin;password=2!5abA%9TqL*R&7;database=YemekTarifi", new MySqlServerVersion(new Version(8, 0, 32)))
+                .UseLoggerFactory(LoggerFactory.Create(b => b
+                .AddConsole()
+                .AddFilter(level => level >= LogLevel.Information)))
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        }
+    }
+}
