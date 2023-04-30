8bdbffb78398f9605c8162f079d1dea7e067ce47
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-04-30 09:21:42 +0300

    Category model created

---
+++ Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YemekTarifleri.DataBase
+{
+    [Table("Tbl_Kullanicilar")]
+    public class User
+    {
+        [Key]
+        [Column("kullaniciId")]
+        public int? id { get; set; }
+        [Column("kullaniciKullaniciAdi")]
+        public String? userName { get; set; }
+        [Column("kullaniciSifre")]
+        public String? password { get; set; }
+        [Column("kullaniciMail")]
+        public String? email { get; set; }
+        [Column("kullaniciAdSoyad")]
+        public String? nameAndSurname { get; set; }
+        public Boolean? userType { get; set; }
+        [Column("kullaniciAvatar")]
+        public String? avatar { get; set; }
+    }
+}
