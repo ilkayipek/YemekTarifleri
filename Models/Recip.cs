79512c66d9db719f7babbbf91b33ba442197e9d6
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-04-30 10:23:35 +0300

    Create Recip model

---
+++ Models/Recip.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YemekTarifleri.Models
+{
+    [Table("Tbl_Tarifler")]
+	public class Recip
+	{
+        [Column("tarifId")]
+        public int? id { get; set; }
+        [Column("tarifAd")]
+        public string? name { get; set; }
+        [Column("tarifMalzeme")]
+        public string? ingredients { get; set; }
+        [Column("tarifYapılış")]
+        public string? cooking { get; set; }
+        [Column("tarifResim")]
+        public string? image { get; set; }
+        [Column("tarifTarih")]
+        public string? date { get; set; }
+        [Column("tarifPuan")]
+        public double? point { get; set; }
+        [Column("kategoriId")]
+        public int? categoryId { get; set; }
+        [Column("kullaniciId")]
+        public int? userId { get; set; }
+        [Column("GununYemegimi")]
+        public bool? isMealOfDay { get; set; }
+    }
+}
+
