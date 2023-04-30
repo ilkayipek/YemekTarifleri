babcd8fe1075225d65045222fde7720d7eeab58d
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-04-30 11:19:57 +0300

    Category fix

--- Models/Category.cs
+++ Models/Category.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace YemekTarifleri.Models
 {
     [Table("Tbl_Kategoriler")]
-    public class Kategori
+    public class Category
     {
         [Key]
         [Column("kategoriId")]
