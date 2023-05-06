a2ef55ba41bd4a134cfce84e468f4beb013982fc
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-05-06 16:31:21 +0300

    Create SearchController.cs

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using YemekTarifleri.DataBase;
+using YemekTarifleri.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace YemekTarifleri.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: /<controller>/
+        [HttpPost]
+        public IActionResult Search(string searchInput)
+        {
+            ApplicationConnectionDb db = new ApplicationConnectionDb();
+            List<Recipe> searchResults = db.recipes
+                .Include(r => r.user) // Kullanıcı bilgilerini ekleyin
+                .Where(r => r.name.Contains(searchInput) || r.ingredients.Contains(searchInput))
+                .ToList();
+
+            ViewBag.SearchResults = searchResults;
+
+            return View();
+        }
+    }
+}
+
