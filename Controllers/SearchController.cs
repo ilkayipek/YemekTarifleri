c244dfd7bbfacf7a8b4d719fec90217e640fc84b
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-05-10 19:27:41 +0300

    search  controller and view fix

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -7,23 +7,24 @@ using Microsoft.EntityFrameworkCore;
 using YemekTarifleri.DataBase;
 using YemekTarifleri.Models;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 
 namespace YemekTarifleri.Controllers
 {
     public class SearchController : Controller
     {
-        // GET: /<controller>/
+        
         [HttpPost]
         public IActionResult Search(string searchInput)
         {
             ApplicationConnectionDb db = new ApplicationConnectionDb();
-            List<Recipe> searchResults = db.recipes
-                .Include(r => r.user) // Kullanıcı bilgilerini ekleyin
-                .Where(r => r.name.Contains(searchInput) || r.ingredients.Contains(searchInput))
-                .ToList();
+            var searchResult = db.recipes
+    .Include(r => r.category)
+    .Include(r => r.user)
+    .Where(r => r.name.Contains(searchInput) || r.category.name.Contains(searchInput))
+    .ToList();
 
-            ViewBag.SearchResults = searchResults;
+            ViewBag.SearchResults = searchResult;
 
             return View();
         }
