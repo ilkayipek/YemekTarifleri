9614f1047d5e9cb068a1824d56d539ff2e92e551
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-05-29 20:06:29 +0300

    Create SessionFilter.cs

---
+++ Controllers/SessionFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+
+public class SessionFilter : IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        string? userId = context.HttpContext.Request.Cookies["userId"];
+
+        if (string.IsNullOrEmpty(userId))
+        {
+            // Oturum kapalı, giriş yapma sayfasına yönlendirme yapabilir veya hata sayfası gösterebilirsiniz
+            //context.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "SignIn", action = "SignIn" }));
+            if (context.ActionDescriptor.RouteValues["controller"] != "Home" && context.ActionDescriptor.RouteValues["action"] != "Index")
+            {
+                //context.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Home", action = "Index" }));
+            }
+        }
+    }
+}
