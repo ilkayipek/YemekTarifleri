628c1e1f9053c4ab7d3fc457b95a6367bfd9f04f
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-05-29 19:50:55 +0300

    Update Comment.cs

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -9,22 +9,21 @@ namespace YemekTarifleri.Models
 		[Key]
 		[Column("yorumId")]
 		public int? id { get; set; }
-		[Column("yorumAdSoyad")]
-		public string? nameAndSurname { get; set; }
-        [Column("yorumMail")]
-        public string? email { get; set; }
         [Column("yorumTarih")]
         public string? date { get; set; }
         [Column("yorumOnay")]
-        public string? approvalStatus { get; set; }
+        public bool? approvalStatus { get; set; }
         [Column("yorumIcerik")]
         public string? contents { get; set; }
         [Column("tarifId")]
-        public int? recipId { get; set; }
+        public int? recipeId { get; set; }
         [Column("kullaniciId")]
         public int? userId { get; set; }
         [Column("yorumPuan")]
         public double? point { get; set; }
+
+        public User user { get; set; }
+        public Recipe recipe { get; set; }
     }
 }
 
