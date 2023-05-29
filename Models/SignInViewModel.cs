7af1bbe7a8e2dd8fc88ab20385135175d24d6894
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-05-29 20:31:39 +0300

    Create SignInViewModel.cs

---
+++ Models/SignInViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace YemekTarifleri.Models
+{
+	public class SignInViewModel
+	{
+		public string? emailOrUsername { get; set; }
+		public string? password { get; set; }
+		public bool IsSubmitted { get; set; }
+
+    }
+}
+
