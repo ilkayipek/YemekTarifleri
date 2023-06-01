0b1ab2eec9a6576fa45dbcb55140556e342a528c
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-06-01 11:10:53 +0300

    Create RegisterViewModel.cs

---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YemekTarifleri.Models
+{
+	public class RegisterViewModel
+	{
+        public String userName { get; set; }
+        public String? password { get; set; }
+        public String? againPassword { get; set; }
+        public String? email { get; set; }
+        public String? nameAndSurname { get; set; }
+        public bool gender { get; set; }
+        public bool provisions { get; set; }
+    }
+}
+
