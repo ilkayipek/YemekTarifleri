2b4d207a74d5415253aa5ba3e0fd381d4a1220df
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-06-01 11:10:43 +0300

    Create RecipeShareViewModel.cs

---
+++ Models/RecipeShareViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace YemekTarifleri.Models
+{
+    public class RecipeShareViewModel
+    {
+        public string? name { get; set; }
+        public int categoryId { get; set; }
+        public IFormFile? image { get; set; }
+        public string? ingredients { get; set; }
+        public string? cooking { get; set; }
+    }
+}
